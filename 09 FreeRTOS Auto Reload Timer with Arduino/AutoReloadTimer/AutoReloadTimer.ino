d28d648722d7f94053a4ea2ee0b884f1067faf21
voidlooprobotech <voidlooprobotech@gmail.com>
2025-11-01 23:23:42 +0530

    Added freertos one shot timer and auto reload timer code

---
+++ 09 FreeRTOS Auto Reload Timer with Arduino/AutoReloadTimer/AutoReloadTimer.ino
@@ -0,0 +1,28 @@
+#include <Arduino_FreeRTOS.h>
+#include "timers.h"
+
+#define ONE_SHOT_PERIOD pdMS_TO_TICKS(5000)
+
+TimerHandle_t xTimer;
+
+void setup() {
+  Serial.begin(9600);
+
+  xTimer = xTimerCreate(
+    "auto reload timer",      // Timer name (optional)
+    ONE_SHOT_PERIOD,          // Timer period (in ticks)
+    pdTRUE,                   // auto-reload Enabled
+    0,                        // Timer ID (not used here)
+    prvAutoReloadTimerCallback   // Callback function
+  );
+
+  xTimerStart(xTimer, 0);     // Start the timer with no block time
+}
+
+void prvAutoReloadTimerCallback(TimerHandle_t xTimer) {
+  Serial.println("Auto Reload timer callback function executed......");
+}
+
+void loop() {
+  // Main loop left empty for FreeRTOS task scheduling
+}
